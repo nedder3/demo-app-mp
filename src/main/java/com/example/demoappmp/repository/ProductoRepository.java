8baed5e7433762edf3f460edd35cae7d980f211d
Nedder3 <arij.dev@gmail.com>
2025-07-17 10:59:06 -0300

    add ProductoRepository

---
+++ src/main/java/com/example/demoappmp/repository/ProductoRepository.java
@@ -0,0 +1,9 @@
+package com.example.demoappmp.repository;
+
+import com.example.demoappmp.model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+}
