ec6f5322bd10080cb18a7c48c9a809fa56962291
Nedder3 <arij.dev@gmail.com>
2025-07-17 10:30:00 -0300

    update app with postconstruct

--- src/main/java/com/example/demoappmp/App.java
+++ src/main/java/com/example/demoappmp/App.java
@@ -1,10 +1,21 @@
 package com.example.demoappmp;
+import com.mercadopago.MercadoPagoConfig;
 
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
+	@Value("${mercado.pago.access.token}")
+	private  String accessToken;
+
+	@PostConstruct
+	public void init(){
+		MercadoPagoConfig.setAccessToken(accessToken);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
