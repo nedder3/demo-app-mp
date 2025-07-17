c6ba05d030a5cb36f00bf75e81f3a2c1dd5224fd
Nedder3 <arij.dev@gmail.com>
2025-07-17 10:42:39 -0300

    update Producto.java

--- src/main/java/com/example/demoappmp/model/Producto.java
+++ src/main/java/com/example/demoappmp/model/Producto.java
@@ -1,4 +1,27 @@
 package com.example.demoappmp.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private String nombre;
+
+    private  String descripcion;
+
+    private Double precio;
 }
